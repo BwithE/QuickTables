8fbf8f6791d98689d46a678345f06c442b0a4731
BwithE <144924113+BwithE@users.noreply.github.com>
2024-10-31 23:51:31 -0400

    Update tables.sh

--- tables.sh
+++ tables.sh
@@ -8,31 +8,10 @@ iptables -t nat -X
 iptables -t mangle -F
 iptables -t mangle -X
 
-
-# Allow loopback interface traffic
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A OUTPUT -o lo -j ACCEPT
-
 # Allow established and related incoming connections
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-
-# Allow incoming SSH (port 22) - adjust if you use a different port
-#iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -j ACCEPT
-
-# Allow incoming HTTP (port 80) and HTTPS (port 443) - if you are running a web server
-# Uncomment the following lines if needed
-# iptables -A INPUT -p tcp --dport 80 -m conntrack --ctstate NEW -j ACCEPT
-# iptables -A INPUT -p tcp --dport 443 -m conntrack --ctstate NEW -j ACCEPT
-
-# Allow incoming ICMP (ping)
-#iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
-
-# Allow other specific ports if needed
-# iptables -A INPUT -p tcp --dport <port_number> -m conntrack --ctstate NEW -j ACCEPT
-
-# Log dropped packets (optional, can be verbose)
-# iptables -A INPUT -m limit --limit 5/min -j LOG --log-prefix "iptables dropped: " --log-level 7
-
+# Allow loopback interface traffic
+iptables -A INPUT -i lo -j ACCEPT
 
 # Set default policies
 iptables -P INPUT DROP
